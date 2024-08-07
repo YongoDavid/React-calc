907b60099ad749ea0cc957c44badbde544dd058b
Yongo David <davidyongo9@gmail.com>
2024-08-07 04:03:11 +0100

    commit

---
+++ src/DigitButton.js
@@ -0,0 +1,6 @@
+import { ACTIONS } from "./Component"
+export default function DigitButton({dispatch , digit}){
+    return(
+        <button onClick={() => dispatch({type: ACTIONS.ADD_DIGIT , payload: {digit}}) }>{digit}</button>
+    )
+}
